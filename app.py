f48d0b297a80e9b45fa59804951706a72556cb40
namratha03 <60363794+namratha03@users.noreply.github.com>
2020-02-27 22:48:02 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -11,12 +11,25 @@ type_defs = load_schema_from_path('schema.graphql')
 query = QueryType()
 student = ObjectType('Student')
 
+queryClass = QueryType()
+classs = ObjectType('Classs')
+
+
 mutation = MutationType()
 mutation.set_field('create_Student', r.resolver_createStudent)
 
-query.set_field('student_with_id', r.student_with_id)
+mutationClass = MutationType()
+mutationClass.set_field('create_Class', r.resolver_createClass)
 
-schema = make_executable_schema(type_defs, [student, mutation, query])
+mutationAddStudentToClass =MutationType()
+mutationAddStudentToClass.set_field('add_Student_to_Class',r.resolver_add_student_to_class)
+
+
+query.set_field('student_with_id', r.student_with_id)
+queryClass.set_field('class_with_id', r.class_with_id)
+
+
+schema = make_executable_schema(type_defs, [student, mutation, query,classs,mutationClass,queryClass,mutationAddStudentToClass])
 
 @app.route('/graphql', methods=['GET'])
 def playground():
